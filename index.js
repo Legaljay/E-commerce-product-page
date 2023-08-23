0018715be683301d809cb2ed3746078911d14f24
Legaljay <adesinaayobami67@gmail.com>
2023-08-23 14:37:30 +0100

    progress on my E-commerce website

---
+++ index.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const decreaseButton = document.querySelector('#btn-decrease');
+    const increaseButton = document.querySelector('#btn-increase');
+    const cartCounter = document.querySelector('#counter');
+    const supElement = document.querySelector('#sup');
+    const emptyCartElement = document.querySelector('.emptyCart');
+    const downCrtElement = document.querySelector('.downCrt');
+    const cartButton = document.querySelector('#cart-btn');
+    const priceElement = document.querySelector('#price');
+    const qttyElement = document.querySelector('#Qtty');
+    const totalElement = document.querySelector('#totale');
+    const cartLi = document.querySelector('#cart');
+    const cartShow = document.querySelector('.show');
+    const deleteButton = document.querySelector('#del-btn');
+
+    function updateSupValue() {
+        supElement.textContent = cartCounter.textContent;
+    }
+    
+
+    function toggleCartElements() {
+        if (parseInt(cartCounter.textContent) === 0) {
+            emptyCartElement.style.display = 'block';
+            downCrtElement.style.display = 'none';
+        } else {
+            emptyCartElement.style.display = 'none';
+            downCrtElement.style.display = 'block';
+        }
+    }
+
+    // toggleCartElements();
+
+    function remove(){
+        const cartItem = deleteButton.closest('.grid');
+        cartItem.remove();
+        cartCounter.textContent = '0';
+        updateSupValue();
+        toggleCartElements();
+        updateItemBreakdown();
+    }
+
+    function updateItemBreakdown() {
+        const itemPrice = parseFloat(priceElement.textContent);
+        const quantity = parseInt(qttyElement.textContent);
+        const totalPrice = itemPrice * quantity;
+        totalElement.textContent = '$' + totalPrice.toFixed(2);
+    }
+
+
+    decreaseButton.addEventListener('click', function () {
+        const currentValue = parseInt(cartCounter.textContent);
+        if (currentValue > 0) {
+            cartCounter.textContent = currentValue - 1;
+            // updateSupValue();
+            // toggleCartElements();
+            qttyElement.textContent = cartCounter.textContent;
+            
+        }
+    });
+
+    increaseButton.addEventListener('click', function () {
+        const currentValue = parseInt(cartCounter.textContent);
+        cartCounter.textContent = currentValue + 1;
+        // updateSupValue();
+        // toggleCartElements();
+        qttyElement.textContent = cartCounter.textContent;
+        // updateItemBreakdown();
+    });
+
+    cartButton.addEventListener('click', function () {
+        supElement.textContent = cartCounter.textContent;
+        qttyElement.textContent = cartCounter.textContent;
+        updateSupValue();
+        updateItemBreakdown();
+        toggleCartElements();
+        updateItemBreakdown();
+    });
+
+    cartLi.addEventListener('click', function () {
+        cartShow.classList.toggle('active');
+    });
+
+    deleteButton.addEventListener('click', remove);
+
+    /**side navigation toggle**/
+
+    function openNav() {
+        document.querySelector(".side-nav").style.width = "250px";
+    }
+    
+    function closeNav() {
+        document.querySelector(".side-nav").style.width = "0";
+    }
+
+    const menuBar = document.querySelector('#menu-bar');
+    const closeMenuBar = document.querySelector('#close-btn');
+
+    menuBar.addEventListener('click', openNav);
+    closeMenuBar.addEventListener('click', closeNav);
+
+});
+
